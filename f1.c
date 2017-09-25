9503c9cb0b5d6e3c8be7dfef47ec10c7be408ab2
Henrique Rocha <hmrocha@gmail.com>
2017-09-25 21:31:56 +0200

    F1 race second version.

--- f1.c
+++ f1.c
@@ -1,26 +1,35 @@
-/* Pontos numa corrida de F1; primeira versão */
+/* Pontos numa corrida de F1; segunda versão */
 
 #include <stdio.h>
 
 int main()
 {
   int place;
+  int pts;
   printf("Qual é o lugar? ");
   scanf("%d", &place);
   if (place == 1)
-    printf("Ganhou 10 pontos.\n");
+    pts = 10;
   else if (place == 2)
-    printf("Ganhou 6 pontos.\n");
+    pts = 6;
   else if (place == 3)
-    printf("Ganhou 4 pontos.\n");
+    pts = 4;
   else if (place == 4)
-    printf("Ganhou 3 pontos.\n");
+    pts = 3;
   else if (place == 5)
-    printf("Ganhou 2 pontos.\n");
+    pts = 2;
   else if (place == 6)
+    pts = 1;
+  else
+    pts = 0;
+
+  if (pts > 1)
+    printf("Ganhou %d pontos.\n", pts);
+  else if (pts == 1)
     printf("Ganhou 1 ponto.\n");
   else
     printf("Não ganhou pontos.\n");
+
   return 0;
 }
 
