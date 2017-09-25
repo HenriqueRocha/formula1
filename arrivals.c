581a3c1139fe9e24f081d4ade6cf2d2cc4fd2cf0
Henrique Rocha <hmrocha@gmail.com>
2017-09-25 22:58:30 +0200

    Better C code style.

--- arrivals.c
+++ arrivals.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
 
 #define MAX_ARRIVALS 26
-int arrivals[MAX_ARRIVALS + 1]; /* index 0 not used */
+int arrivals[MAX_ARRIVALS];
 int n_arrivals;
 
-int get_arrivals(void);
+int get_arrivals (void);
 
-main()
+int main (void)
 {
-  n_arrivals = get_arrivals();
+  get_arrivals();
   printf("Número de chegadas:    %2d\n"
          "Número de desistentes: %2d\n",
          n_arrivals, MAX_ARRIVALS - n_arrivals);
   return 0;
 }
 
-int get_arrivals(void)
+int get_arrivals (void)
 {
-  int i;
   int number;
-  for (i = 1; i <= MAX_ARRIVALS; ++i)
-    {
-      printf("%2dº. lugar (0 para terminar): ", i);
-      scanf("%d", &number);
-      if (!number)
-        break;
-      arrivals[i] = number;
-    }
-  return i - 1;
+  int i;
+  for (i = 0; i < MAX_ARRIVALS; i++)
+  {
+    printf("%2dº. lugar (0 para terminar): ", i + 1);
+    scanf("%d", &number);
+    if (!number)
+      break;
+    arrivals[i] = number;
+  }
+  n_arrivals = i;
 }
 
